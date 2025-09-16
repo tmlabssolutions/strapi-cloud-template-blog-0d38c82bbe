4698b7768f01e5b7a5595a1c36512c2e57bf2fff
John Uguru <johnuguru73@gmail.com>
2025-09-16 13:18:35 +0100

    fix: default limits

--- config/plugins.js
+++ config/plugins.js
@@ -1,15 +1,16 @@
 module.exports = () => ({
-    graphql: {
-        enabled: true,
-        config: {
-          endpoint: '/graphql',
-          shadowCRUD: true,         // auto-generate queries/mutations
-          playgroundAlways: false,  // show playground in non-production only
-          depthLimit: 7,            // guard against deeply nested queries
-          amountLimit: 100,         // max items per query
-          apolloServer: {
-            tracing: false,         // enable Apollo tracing if needed
-          },
-        },
+  graphql: {
+    enabled: true,
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,         // auto-generate queries/mutations
+      playgroundAlways: false,  // show playground in non-production only
+      depthLimit: 50,            // guard against deeply nested queries
+      amountLimit: 100,         // max items per query
+      apolloServer: {
+        tracing: false,         // enable Apollo tracing if needed
       },
+      defaultLimit: 50,
+    },
+  },
 });
